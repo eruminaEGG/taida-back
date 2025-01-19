edfc2d0360378a823d906da31b92ce453d3210ed
erumina <EruminaBusiness@gmail.com>
2025-01-19 17:25:16 +0900

    URLを修正

--- utils/datetime_util.py
+++
@@ -1,9 +0,0 @@
-from datetime import datetime, timedelta, timezone
-
-
-class DatetimeUtil:
-    JST: timezone = timezone(timedelta(hours=9))
-
-    @staticmethod
-    def get_current_time() -> datetime:
-        return datetime.now(tz=DatetimeUtil.JST)
